fb201412e92cfeec782929e711979323e8cd566e
lone4alker <anoopshetty47@gmail.com>
2025-11-17 20:55:31 +0530

    shadcn

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+      <h1 className="text-3xl">Ggs</h1>
+    </div>
+  )
+}
+
+export default App
